f94e3469ca0ff6fe1e3d2198e1379c06f408db6a
Sydney Orr <sydneywhitters@gmail.com>
2021-09-21 14:32:17 -0600

    init

---
+++ src/Navbar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { NavLink, } from "react-router-dom";
+import { Menu, } from "semantic-ui-react";
+import { UserConsumer } from "./UserProvider";
+
+const Navbar = () => (
+  <UserConsumer>
+    { value => (
+      <Menu>
+        <NavLink to="/">
+          <Menu.Item>
+            Home
+          </Menu.Item>
+        </NavLink>
+      </Menu> 
+    )}
+  </UserConsumer>
+)
+
+export default Navbar;
---
+++ src/UserProvider.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+const UserContext = React.createContext();
+
+export const UserConsumer = UserContext.Consumer
+
+class UserProvider extends React.Component {
+  state =  {
+    email: "test@gmail.com",
+    firstName: "sydney",
+    lastName: "orr",
+    avatar: "avatar",
+  };
+
+  render() {
+    return (
+      <UserContext.Provider value = {this.state}>
+        {this.props.children}
+      </UserContext.Provider>
+    )
+  }
+}
+
+export default UserProvider;
